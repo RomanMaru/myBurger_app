5c717edf2ad25692e61c09b3f68d7be4082f2e5c
Roman Maru <romanmaru10@gmail.com>
2019-10-03 11:46:05 -0700

    Add guarding routes

--- src/App.js
+++ src/App.js
@@ -1,31 +1,49 @@
 import React, { Component } from 'react';
-import { Route, Switch, withRouter } from 'react-router-dom';
+import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
 import Auth from './containers/Auth/Auth'
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
 import Orders from './containers/Orders/Orders';
 import Logout from './containers/Auth/Logout/Logout'
-import {connect } from 'react-redux'
+import { connect } from 'react-redux'
 import * as actions from './store/actions/index'
 class App extends Component {
   componentDidMount() {
     this.props.onTryAutoSingUp()
   }
-  render () {
+  render() {
+    let routes = (
+      <Switch>
+        <Route path="/auth" component={Auth} />
+        <Route path="/" exact component={BurgerBuilder} />
+        <Redirect to='/' />
+      </Switch>
+    )
+
+    if (this.props.isAuthenticated) {
+      routes = (
+        <Switch>
+          <Route path="/checkout" component={Checkout} />
+          <Route path="/orders" component={Orders} />
+          <Route path="/logout" component={Logout} />
+          <Route path="/" exact component={BurgerBuilder} />
+        </Switch>
+      )
+    }
     return (
       <div>
         <Layout>
-          <Switch>
-            <Route path="/checkout" component={Checkout} />
-            <Route path="/orders" component={Orders} />
-            <Route path="/auth" component={Auth} />
-            <Route path="/logout" component={Logout} />
-            <Route path="/" exact component={BurgerBuilder} />
-          </Switch>
+          {routes}
         </Layout>
       </div>
     );
+  }
+}
+
+const mapStateToprops = state => {
+  return {
+    isAuthenticated: state.auth.token !== null
   }
 }
 
@@ -34,4 +52,4 @@ const mapDispatchToProps = dispatch => {
     onTryAutoSingUp: () => dispatch(actions.authCheckState())
   }
 }
-export default  withRouter(connect(null, mapDispatchToProps)(App))
+export default withRouter(connect(mapStateToprops, mapDispatchToProps)(App))
