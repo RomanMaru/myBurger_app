a7e130b7433de428c7f16ec405982e63f3a1c34f
Roman Maru <romanmaru10@gmail.com>
2019-10-08 17:41:19 -0700

    Write tests for auth reducer and BuildControls Component

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -1,14 +1,29 @@
 import React from 'react'
 import { configure, shallow } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
-import NavigationItems from './NavigationItems.js'
-import NavigationItem from './NavigationItem'
+import NavigationItems from './NavigationItems'
+import NavigationItem from './NavigationItem/NavigationItem'
 
-configure({ adapter: new Adapter() })
+configure({ adapter: new Adapter() });
 
-describe('<NavigationIItems />', () => {
+describe('<NavigationItems />', () => {
+    let wrapper
+
+    beforeEach(() => {
+        wrapper = shallow(<NavigationItems />)
+    })
+
     it('should render two <NavigationItem /> elements if not aithenticated', () => {
-        const wrapper = shallow(<NavigationItems />)
         expect(wrapper.find(NavigationItem)).toHaveLength(2)
+    })
+
+    it('should render two <NavigationItem /> elements if not aithenticated', () => {
+        wrapper.setProps({ isAuthenticated: true })
+        expect(wrapper.find(NavigationItem)).toHaveLength(3)
+    })
+
+    it('should render three <NavigationItem /> elements if aithenticated', () => {
+        wrapper.setProps({ isAuthenticated: true })
+        expect(wrapper.contains(<NavigationItem link="/logout">Logout</NavigationItem>)).toEqual(true)
     })
 })
---
+++ src/store/reducers/auth.test.js
@@ -0,0 +1,35 @@
+import reducer from './auth'
+import * as actionTypes from '../actions/actionTypes'
+import { isMainThread } from 'worker_threads'
+
+describe('auth reducer', () => {
+    it('should return the initial state', () => {
+        expect(reducer(undefined, {})).toEqual({
+            token: null,
+            userId: null,
+            error: null,
+            loading: false,
+            authRedirectPath: '/'
+        })
+    })
+
+    it('should store the token upon login', () => {
+        expect(reducer({
+            token: null,
+            userId: null,
+            error: null,
+            loading: false,
+            authRedirectPath: '/'
+        }, {
+            type: actionTypes.AUTH_SUCCESS,
+            idToken: 'some token',
+            userId: 'some id'
+        })).toEqual({
+            token: 'some token',
+            userId: 'some id',
+            error: null,
+            loading: false,
+            authRedirectPath: '/'
+        })
+    })
+})
